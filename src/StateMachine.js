e1bc1e76e353e7429073008f58eb34fa8dc14beb
Daniel Hammerschmidt <daniel@redneck-engineering.com>
2016-08-23 11:33:22 +0200

    [FIX] typo

--- src/StateMachine.js
+++ src/StateMachine.js
@@ -22,35 +22,35 @@ class StateMachine {
     });
     
     var index = -1;
-    var next = function __statem_machine_next() {
+    var next = function __state_machine_next() {
       setTimeout( states[ ++index ].bind( this, next, ...arguments ), 0 );
     }
     Object.defineProperties( next, {
-      skip: { value: function __statem_machine_skip( num = 1 ) {
+      skip: { value: function __state_machine_skip( num = 1 ) {
         index += num;
         return next.bind( this );
       }},
-      jump: { value: function __statem_machine_jump( index_or_name ) {
+      jump: { value: function __state_machine_jump( index_or_name ) {
         if ( typeof index_or_name === 'string' ) {
           index_or_name = indexesByName[ index_or_name ];
         }
         index = index_or_name - 1;
         return next.bind( this );
       }},
-      try: { value: function __statem_machine_try() {
+      try: { value: function __state_machine_try() {
         var [ thisArg, args ] = [ this, arguments ]; 
         return {
           catch: ( error_callback_or_name )=>{
             if ( typeof error_callback_or_name === 'string' ) {
               error_callback_or_name = states[ indexesByName[ error_callback_or_name ] ];
             }
-            setTimeout( function __statem_machine_try_catch() {
+            setTimeout( function __state_machine_try_catch() {
               try {
-                // ( function __statem_machine_try() {
+                // ( function __state_machine_try() {
                   states[ ++index ].call( thisArg, next, ...args );
                 // })();
               } catch ( e ) {
-                // ( function __statem_machine_catch() {
+                // ( function __state_machine_catch() {
                   error_callback_or_name.call( thisArg, e, next, ...args );
                 // })();
               }
@@ -58,35 +58,35 @@ class StateMachine {
           },
         };
       }},
-      atomic: { value: function __statem_machine_atomic_next() {
+      atomic: { value: function __state_machine_atomic_next() {
         states[ ++index ].call( this, next, ...arguments );
       }},
     });
     Object.defineProperties( next.atomic, {
-      skip: { value: function __statem_machine_atomic_skip( num = 1 ) {
+      skip: { value: function __state_machine_atomic_skip( num = 1 ) {
         index += num;
         return next.atomic.bind( this );
       }},
-      jump: { value: function __statem_machine_atomic_jump( index_or_name ) {
+      jump: { value: function __state_machine_atomic_jump( index_or_name ) {
         if ( typeof index_or_name === 'string' ) {
           index_or_name = indexesByName[ index_or_name ];
         }
         index = index_or_name - 1;
         return next.atomic.bind( this );
       }},
-      try: { value: function __statem_machine_atomic_try() {
+      try: { value: function __state_machine_atomic_try() {
         var [ thisArg, args ] = [ this, arguments ]; 
         return {
-          catch: function __statem_machine_atomic_try_catch( error_callback_or_name ) {
+          catch: function __state_machine_atomic_try_catch( error_callback_or_name ) {
             if ( typeof error_callback_or_name === 'string' ) {
               error_callback_or_name = states[ indexesByName[ error_callback_or_name ] ];
             }
             try {
-              // ( function __statem_machine_atomic_try() {
+              // ( function __state_machine_atomic_try() {
                 states[ ++index ].call( thisArg, next, ...args );
               // })();
             } catch( e ) {
-              // ( function __statem_machine_atomic_catch() {
+              // ( function __state_machine_atomic_catch() {
                 error_callback_or_name.call( thisArg, e, next, ...args );
               // })();
             }
